9446fc6eff5c9c1283b8bc6aed5baa2f9c81ee59
Mr.devil <103495827+Mrdevilbrand@users.noreply.github.com>
2022-05-20 15:56:32 +0500

    Updated

--- dvl.py
+++ dvl.py
@@ -38,19 +38,7 @@ def runtxt(z):
         time.sleep(0.03)
  
  
- 
-def main():
- 	
 	
-plist = (platform.uname())[2]
-basex = plist
-basex1 = basex.encode('ascii')
-basex2 = base64.b64encode(basex1)
-basex3 = basex2.decode('ascii')
-base4 = (basex3).upper()
-basesplit = base4.replace('=', 'X').replace('A', '3').replace('B', '9').replace('C', '7').replace('D', '1').replace('E', '4').replace('M', '2').replace('L', '6').replace('F', '8').replace('N', 'E').replace('T', '8')
- 
- 
 class Main:
 	def __init__(self):
 		self.id = []

c039044493e7aa9edf6f3777928c466ba8d10b9a
Mr.devil <103495827+Mrdevilbrand@users.noreply.github.com>
2022-05-20 10:32:18 +0500

    Updated

--- lov.py
+++ lov.py
@@ -25,12 +25,7 @@ br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
- 
-def exb():
-    print '[!] Exit Successfully '
-    os.sys.exit()
- 
- 
+  
 def acak(b):
     w = 'ahtdzjc'
     d = ''

